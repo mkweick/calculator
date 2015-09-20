14ae433b1b72cf33c4ec276ace400172582e3151
Matt Weick <mkweick@gmail.com>
2015-09-20 18:34:21 +0000

    Finalize calculator program

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator
+
+Building a Ruby calculator
+
+First Assignment in Lesson 1 of Introduction to Ruby & Web Development on Tealeaf
+
+Matt
---
+++ calculator.rb
@@ -0,0 +1,54 @@
+puts "Ruby Calculator"
+puts
+puts "----- addition       (+)"
+puts "----- subtraction    (-)"
+puts "----- multiplication (*)"
+puts "----- division       (/)"
+puts "----- squared        (**)"
+puts "** Must have a space before and after operator"
+puts
+puts "Example format: 10 * 2  --or--  5 + 10"
+
+begin
+  puts "Enter math operation below:"
+
+  # Get user Math statement 
+  user_array = gets.chomp.split
+
+  # Check to make sure statement has valid operator
+  while user_array.none? { |symbol| ["+", "-", "*", "/", "**"].include?(symbol) }
+    puts "You have not entered a valid math operator."
+    puts "Please ensure you have a space before and after operator."
+    puts "Valid operators are:"
+    puts
+    puts "-->  +   addition"
+    puts "-->  -   subtraction"
+    puts "-->  *   multiplication"
+    puts "-->  /   division"
+    puts "-->  **  squared"
+    puts
+    puts "Try again:"
+    user_array = gets.chomp.split
+  end
+
+  first_number  = user_array[0].to_i
+  operator      = user_array[1]
+  second_number = user_array[2].to_i
+
+  # Execute correct math operation based on operator
+  case operator
+  when "+"
+    puts "Result: #{first_number + second_number}"
+  when "-"
+    puts "Result: #{first_number - second_number}"
+  when "*"
+    puts "Result: #{first_number * second_number}"
+  when "/"
+    puts "Result: #{first_number / second_number}"
+  when "**"
+    puts "Result: #{first_number ** second_number}"
+  end
+
+  puts "New math operation? (Y/N)"
+  new_operation = gets.chomp.upcase
+end until new_operation != 'Y'
